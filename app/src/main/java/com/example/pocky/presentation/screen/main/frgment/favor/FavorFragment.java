016404dcf1d328c8e6ae7067b79a782f033ea274
centerfoward <jsu5438@naver.com>
2024-09-15 17:47:24 +0900

    [FEAT] Apply RecycerView in FavorFragment

--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/favor/FavorFragment.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/favor/FavorFragment.java
@@ -1,31 +1,89 @@
 package com.example.pocky.presentation.screen.main.frgment.favor;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.pocky.R;
+import com.example.pocky.databinding.FragmentFavorBinding;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class FavorFragment extends Fragment {
+
+    private  FragmentFavorBinding binding;
+    private FavorModalBottomsheet bottomsheet;
+    private FavorViewModel viewModel ;
+    private FavorAdapter favorAdapter;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        //바인딩 초기화
+        binding = FragmentFavorBinding.inflate(inflater,container,false);
 
+        //QR 바텀 다이얼로그 초기화
+        bottomsheet = new FavorModalBottomsheet();
 
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //뷰모델 초기화
+        viewModel = new ViewModelProvider(this).get(FavorViewModel.class);
+
+        // ViewModel에서 데이터를 가져와서 RecyclerView에 반영
+        viewModel.getFavorList().observe(getViewLifecycleOwner(), favors -> {
+            favorAdapter.submitList(favors);  // 데이터를 어댑터에 설정
+        });
+
+        // RecyclerView와 FavorAdapter 초기화
+        favorAdapter = new FavorAdapter();
+        binding.favorRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.favorRecyclerView.setAdapter(favorAdapter);
+
+        // ViewModel에서 데이터를 가져와서 RecyclerView에 반영
+        viewModel.getFavorList().observe(getViewLifecycleOwner(), favors -> {
+            favorAdapter.submitList(favors);  // 데이터를 어댑터에 설정
+        });
+
+        //QR 버튼 정의
+        binding.orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomsheet.show(getParentFragmentManager(),FavorModalBottomsheet.TAG);
+                try {
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.encodeBitmap("Hello, world!", BarcodeFormat.QR_CODE, 300, 300);
+                    ImageView imageViewQrcode = requireView().findViewById(R.id.imageQrCode);
+                    imageViewQrcode.setImageBitmap(bitmap);
+                }catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        // 바인딩 해제
+        binding = null;
     }
 }
--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/favor/FavorViewModel.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/favor/FavorViewModel.java
@@ -1,47 +1,37 @@
 package com.example.pocky.presentation.screen.main.frgment.favor;
 
-import android.util.Log;
-
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.pocky.domain.model.RetrofitService;
-import com.example.pocky.domain.model.favor.FavorDTO;
-import com.example.pocky.domain.model.favor.FavorInterface;
+import com.example.pocky.R;
+import com.example.pocky.domain.model.Favor.Favor;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class FavorViewModel extends ViewModel {
-    private static final FavorInterface favorList = RetrofitService.getInstance().getRetrofit().create(FavorInterface.class);
-    private MutableLiveData<FavorDTO> FavorList;
-    public MutableLiveData<FavorDTO> _FavorList() {
-        if (FavorList == null) {
-            FavorList = new MutableLiveData<FavorDTO>();
-        }
-        return FavorList;
+    private final MutableLiveData<List<Favor>> favorList = new MutableLiveData<>();
+
+    public FavorViewModel() {
+        //TODO Room 데이터 Read, Create 작업 만들기
+        List<Favor> sampleData = new ArrayList<>();
+        sampleData.add(new Favor(R.drawable.resize_foldfork,
+                "메뉴이름", 1,
+                "빵 : 플렛브래드", "소스 : 소스1소스2소스2","토핑 : 토핑토핑토핑토핑토핑토핑",
+                "사이드: 더블 초코칩쿠키","음료 : 사이다"));
+        sampleData.add(new Favor(R.drawable.resize_foldfork,
+                "메뉴이름", 1,
+                "빵", "소스","토핑","사이드","음료"));
+        sampleData.add(new Favor(R.drawable.resize_foldfork,
+                "메뉴이름", 1,
+                "빵", "소스","토핑","사이드","음료"));
+        favorList.setValue(sampleData);
     }
 
-    public void getFavorList(){
-        Call<List<FavorDTO>> list = favorList.getFavor("testid");
-       list.enqueue(new Callback<List<FavorDTO>>() {
-           @Override
-           public void onResponse(Call<List<FavorDTO>> call, Response<List<FavorDTO>> response) {
-               FavorList  = (MutableLiveData<FavorDTO>) response.body();
-               Log.d("FavorViewModel","즐겨찾기 정보 호출 실패");
-           }
-
-           @Override
-           public void onFailure(Call<List<FavorDTO>> call, Throwable t) {
-                Log.d("FavorViewModel","즐겨찾기 정보 호출 실패");
-           }
-       });
+    public LiveData<List<Favor>> getFavorList() {
+        return favorList;
     }
-
-
 
 
 }
