07c079a388510f0b1c546f6ec2a485534ea7c50b
cdjsdjwkfgkrhtlvek <jmprince7@naver.com>
2024-09-22 10:53:03 +0900

    UI work

---
+++ app/src/main/java/com/example/pocky/ConfirmationActivity.java
@@ -0,0 +1,14 @@
+package com.example.pocky;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+    }
+}
