21ef4fb1ba9a73b37b0b3c80a12b5e293e8f279a
centerfoward <jsu5438@naver.com>
2024-09-25 00:07:00 +0900

    [FEAT] Wrapp

--- app/src/main/java/com/example/pocky/presentation/screen/order/wrapp/WrappActivity.java
+++ app/src/main/java/com/example/pocky/presentation/screen/order/wrapp/WrappActivity.java
@@ -1,83 +1,97 @@
 package com.example.pocky.presentation.screen.order.wrapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.pocky.R;
+import com.example.pocky.databinding.ActivityWrappBinding;
+import com.example.pocky.domain.model.menu.Menu;
+import com.example.pocky.domain.model.menu.MenuSingleton;
+import com.example.pocky.presentation.screen.order.common.drink.DrinkActivity;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class WrappActivity extends AppCompatActivity {
+    private static List<Integer> imageList;
+    private static List<String> wrappName;
+    private ActivityWrappBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wrapp);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        binding = ActivityWrappBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+        init();
+
+        Menu arr = MenuSingleton.getInstance();
+
+        RecyclerView recyclerView = findViewById(R.id.wrapp_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        List<Integer> imageList = Arrays.asList(
+
+        WrappAdapter adapter = new WrappAdapter(imageList,wrappName, new WrappAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int imageResId, String sideName) {
+                //클릭된 메뉴 이미지, 메뉴 이미지 싱글톤 객체에 담기
+                arr.setMenuName(sideName);
+                arr.setMenuImage(imageResId);
+
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+        //선택 완료
+        binding.confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(arr.getMenuName() == null || arr.getMenuName().isEmpty()){
+                    Toast myToast = Toast.makeText(getApplicationContext(),"먼저 제품을  선택해주세요", Toast.LENGTH_SHORT);
+                    myToast.show();
+                }else{
+                    Log.d("BreakFastActivity","최종적으로 선택된 아이템 : " + arr.getMenuName()+" "+ arr.getMenuImage());
+                    Intent intent = new Intent(getApplicationContext(), DrinkActivity.class); // 아침메뉴
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        //뒤로가기 버튼
+        binding.backspaceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arr.setMenuName("");
+                arr.setQrMenuName("");
+                finish();
+            }
+        });
+    }
+
+    void init(){
+        // 샌드위치 이름, 이미지 초기화
+        imageList = Arrays.asList(
                 R.drawable.resize_chickenbaconminiwrap,
                 R.drawable.resize_shrimpeggmayorap,
                 R.drawable.resize_steakncheeseavocadowrap
         );
 
-        BreadAdapter adapter = new BreadAdapter(imageList);
-        recyclerView.setAdapter(adapter);
-    }
-
-    public class BreadAdapter extends RecyclerView.Adapter<BreadAdapter.BreadViewHolder> {
-
-        private final List<Integer> imageList;
-
-        public BreadAdapter(List<Integer> imageList) {
-            this.imageList = imageList;
-        }
-
-        @Override
-        public BreadViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_long, parent, false);
-            return new BreadViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(BreadViewHolder holder, int position) {
-            Glide.with(holder.itemView.getContext())
-                    .load(imageList.get(position))
-                    .into(holder.imageView);
-
-            holder.imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // 클릭시 이벤트 발생 ( 추가 예정 )
-                }
-            });
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return imageList.size();
-        }
-
-        class BreadViewHolder extends RecyclerView.ViewHolder {
-            ImageView imageView;
-
-            public BreadViewHolder(View itemView) {
-                super(itemView);
-                imageView = itemView.findViewById(R.id.LongImageView);
-            }
-        }
+        wrappName = Arrays.asList(
+                "치킨베이컨미니랩",
+                "쉬림프마요랩",
+                "스테이크치즈아보카도랩"
+        );
     }
 }
