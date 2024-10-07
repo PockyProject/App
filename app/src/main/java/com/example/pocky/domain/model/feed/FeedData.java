deed54b64a831af640f7ca56f59bd3b4f5a0e8ac
centerfoward <jsu5438@naver.com>
2024-10-07 19:29:16 +0900

    [UI] Feat MyPageFragments UI

---
+++ app/src/main/java/com/example/pocky/domain/model/feed/FeedData.java
@@ -0,0 +1,137 @@
+package com.example.pocky.domain.model.feed;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.sql.Timestamp;
+
+public class FeedData {
+
+    @SerializedName("feedUid")
+    private String feedUid; // 기본키
+
+    @SerializedName("userUid")
+    private String userUid;
+
+    @SerializedName("profileImage")
+    private String profileImage;
+
+    @SerializedName("menuImage")
+    private int menuImage;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("content")
+    private String content;
+    @SerializedName("likeCount")
+    private int likeCount;
+    @SerializedName("writedDate")
+    private Timestamp writedDate;
+    @SerializedName("deleteAt")
+    private Timestamp deleteAt;
+    @SerializedName("updateAt")
+    private Timestamp updateAt;
+
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+    public int getMenuImage() {
+        return menuImage;
+    }
+
+    public String getFeedUid() {
+        return feedUid;
+    }
+
+    public void setFeedUid(String feedUid) {
+        this.feedUid = feedUid;
+    }
+
+    public String getUserUid() {
+        return userUid;
+    }
+
+    public void setUserUid(String userUid) {
+        this.userUid = userUid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public Timestamp getWritedDate() {
+        return writedDate;
+    }
+
+    public void setWritedDate(Timestamp writedDate) {
+        this.writedDate = writedDate;
+    }
+
+    public Timestamp getDeleteAt() {
+        return deleteAt;
+    }
+
+    public void setDeleteAt(Timestamp deleteAt) {
+        this.deleteAt = deleteAt;
+    }
+
+    public Timestamp getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Timestamp updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+
+    public void setMenuImage(int menuImage) {
+        this.menuImage = menuImage;
+    }
+
+    public FeedData(String feedUid, String userUid, String title, String content, int likeCount, Timestamp writedDate, Timestamp deleteAt, Timestamp updateAt) {
+        this.feedUid = feedUid;
+        this.userUid = userUid;
+        this.title = title;
+        this.content = content;
+        this.likeCount = likeCount;
+        this.writedDate = writedDate;
+        this.deleteAt = deleteAt;
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // 동일한 객체일 경우
+        if (obj == null || getClass() != obj.getClass()) return false; // 클래스 타입이 다르면 다름
+
+        FeedData feedData = (FeedData) obj; // 형변환
+
+        // 피드 UID를 비교하여 맞는지 확인
+        return feedUid.equals(feedData.getFeedUid());
+    }
+
+
+}
--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/mypage/MypageFragment.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/mypage/MypageFragment.java
@@ -1,5 +1,6 @@
 package com.example.pocky.presentation.screen.main.frgment.mypage;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.bumptech.glide.Glide;
 import com.example.pocky.databinding.FragmentMypageBinding;
+import com.example.pocky.domain.model.feed.FeedData;
+import com.example.pocky.domain.model.user.UserInfo;
+import com.example.pocky.domain.repository.favor.Favor;
+import com.example.pocky.presentation.screen.main.frgment.favor.FavorAdapter;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MypageFragment extends Fragment {
     private FragmentMypageBinding binding;
+    private MypageAdapter adapter;
+    private Context context;
 
     @Nullable
     @Override
@@ -21,4 +34,77 @@ public class MypageFragment extends Fragment {
 
         return binding.getRoot();
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+       // 더미데이터
+        FeedData ex = new FeedData(
+                "feeduid",
+                "useruid",
+                "title",
+                "content",
+                1,
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis())
+        );
+        FeedData ex1 = new FeedData(
+                "feeduid",
+                "useruid",
+                "title",
+                "content",
+                1,
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis())
+        );
+        FeedData ex2 = new FeedData(
+                "feeduid",
+                "useruid",
+                "title",
+                "content",
+                1,
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis()),
+                new Timestamp(System.currentTimeMillis())
+        );
+
+        List<FeedData> feedData = new ArrayList<>();
+        feedData.add(ex);
+        feedData.add(ex1);
+        feedData.add(ex2);
+
+        //어댑터 초기화
+        initAdapter(feedData);
+
+        //프로필 화면 초기화
+        initProfile();
+    }
+
+
+    private void initAdapter(List<FeedData> data){
+        adapter = new MypageAdapter(new FavorAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Favor favor) {
+
+            }
+        });
+        adapter.submitList(data);
+        binding.feedRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.feedRecyclerView.setAdapter(adapter);
+    }
+
+    private void initProfile(){
+        String image = UserInfo.getInstance().getProfileURl();
+        Glide.with(getContext())
+                .load(UserInfo.getInstance().getProfileURl())
+                .circleCrop()
+                .into(binding.profileImageView);
+
+    }
+
+
 }
