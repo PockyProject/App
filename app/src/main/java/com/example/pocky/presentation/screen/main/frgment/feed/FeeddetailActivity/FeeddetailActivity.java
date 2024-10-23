7a78ba9e526289e30cad334fcd905011217bf5d9
centerfoward <jsu5438@naver.com>
2024-10-23 15:49:59 +0900

    [FEAT] Feat View Feed Data in FeedDetail

--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeeddetailActivity.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeeddetailActivity.java
@@ -2,41 +2,43 @@ package com.example.pocky.presentation.screen.main.frgment.feed.FeeddetailActivi
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.example.pocky.R;
+import com.example.pocky.databinding.ActivityFeeddetailBinding;
+import com.example.pocky.domain.model.feed.FeedData;
 
 public class FeeddetailActivity extends AppCompatActivity {
+
+    private ActivityFeeddetailBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feeddetail); // Feeddetail 화면의 레이아웃 설정
+        // 뷰 초기화
+        initView();
 
-        ImageView mainImage = findViewById(R.id.mainImage);
-        TextView maintitleText = findViewById(R.id.titleText);
-        TextView mainContent = findViewById(R.id.maincomment);
+        // 피드 데이터 초기화
+        initData();
+    }
 
+    private void initView(){
+        binding = ActivityFeeddetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // Feeddetail 화면의 레이아웃 설정
+    }
+
+    private void initData(){
         Intent intent = getIntent();
-        int menuImage = intent.getIntExtra("menuImage", R.drawable.baseline_arrow_back_24); // 기본 이미지 설정
-        String titleText = intent.getStringExtra("titleText");
-        String mainText = intent.getStringExtra("mainText");
+        FeedData data = (FeedData) intent.getSerializableExtra("FeedData");
+
         // 전달받은 이미지를 표시
         Glide.with(this)
-                .load(menuImage)
-                .into(mainImage);
-        maintitleText.setText(titleText);
-        mainContent.setText(mainText);
+                .load(data.getMenuImage())
+                .into(binding.mainImage);
 
-
-
-
+        binding.maincomment.setText(data.getTitle());
+        binding.titleText.setText(data.getContent());
     }
 }
