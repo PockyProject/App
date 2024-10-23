99f96ef4f665f88bbdcf80365b5a8b158f599e83
centerfoward <jsu5438@naver.com>
2024-10-23 17:10:12 +0900

    [FEAT] Feat View QrCode

--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeedDetailViewModel.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeedDetailViewModel.java
@@ -1,12 +1,30 @@
 package com.example.pocky.presentation.screen.main.frgment.feed.FeeddetailActivity;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 public class FeedDetailViewModel extends AndroidViewModel {
+
+
     public FeedDetailViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public Bitmap decodeBase64ToBitmap(String base64String) {
+        try {
+            // Base64.NO_WRAP을 사용하여 줄바꿈 문자 문제 방지
+            byte[] decodedString = Base64.decode(base64String, Base64.NO_WRAP);
+            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            Log.e("DecodeError", "Base64 문자열이 잘못되었습니다: " + base64String);
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeeddetailActivity.java
+++ app/src/main/java/com/example/pocky/presentation/screen/main/frgment/feed/FeeddetailActivity/FeeddetailActivity.java
@@ -2,17 +2,24 @@ package com.example.pocky.presentation.screen.main.frgment.feed.FeeddetailActivi
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.example.pocky.databinding.ActivityFeeddetailBinding;
 import com.example.pocky.domain.model.feed.FeedData;
+import com.example.pocky.presentation.screen.main.frgment.favor.FavorModalBottomsheet;
 
 public class FeeddetailActivity extends AppCompatActivity {
 
     private ActivityFeeddetailBinding binding;
+    private FeedDetailViewModel viewModel;
+    private FavorModalBottomsheet bottomsheet;
+    private FeedData data;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,13 +32,31 @@ public class FeeddetailActivity extends AppCompatActivity {
     }
 
     private void initView(){
+
+        // 뷰 초기화
         binding = ActivityFeeddetailBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot()); // Feeddetail 화면의 레이아웃 설정
+        setContentView(binding.getRoot());
+
+        // 뷰모델 초기화
+        FeedDetailViewModelFactory factory = new FeedDetailViewModelFactory(getApplication());
+        viewModel = new ViewModelProvider(this, factory).get(FeedDetailViewModel.class);
+
+        //QR 바텀 다이얼로그 초기화
+        bottomsheet = new FavorModalBottomsheet();
+
+        binding.feedqrOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // qr 띄우기
+                bottomsheet.setQrBitmap(viewModel.decodeBase64ToBitmap(data.getQrImage()));
+                bottomsheet.show(getSupportFragmentManager(),bottomsheet.getTag());
+            }
+        });
     }
 
     private void initData(){
         Intent intent = getIntent();
-        FeedData data = (FeedData) intent.getSerializableExtra("FeedData");
+        data = (FeedData) intent.getSerializableExtra("FeedData");
 
         // 전달받은 이미지를 표시
         Glide.with(this)
